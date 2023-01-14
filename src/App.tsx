bf15da64e668c20644837554f573a52a934d05e1
YasNvd <yaasnvd@yahoo.com>
2023-01-14 09:46:05 +0330

    Login

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter ,Route,Routes} from 'react-router-dom'
 import Layout from './components/Layout';
+import Login from './components/Login';
+import Signup from './components/Signup';
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Layout />} >
+          <Route path='/signin' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+
+
 
           </Route>
         </Routes>
