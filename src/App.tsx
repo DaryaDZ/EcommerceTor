11fff5d610f7b6d81a841275448c4f802c7d2815
nastaran <ndaryaeii.z@gmail.com>
2023-01-14 13:28:57 +0330

    sing up Ui

--- src/App.tsx
+++ src/App.tsx
@@ -10,16 +10,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Layout />} >
-<<<<<<< HEAD
           <Route path='signin' element={<Login />} />
           <Route path='signup' element={<Signup />} />
 
-
-
-=======
-          <Route path='/signin' element={<Login />} />
-          <Route path='/signup' element={<Signup />} />
->>>>>>> 157164b4d52c8fafe3cf17a453aea2be7cbe8704
           </Route>
         </Routes>
       </BrowserRouter>
--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -1,11 +1,72 @@
-import React from 'react'
+import React from "react";
+import {
+  Avatar,
+  Grid,
+  Paper,
+  TextField,
+  Typography,
+  Button,
+} from "@material-ui/core";
 
 const Signup = () => {
+  const paperStyle = {
+    padding: 20,
+    height: "70vh",
+    margin: "20px auto",
+    width: 280,
+  };
+  const headerStyle = {
+    margin: 10,
+  };
+  const avatarStyle = {
+    backgroundColor: "#FFC4C4",
+  };
   return (
-    <div>
-      SINGINGGG
-    </div>
-  )
-}
+    <Grid>
+      <Paper elevation={20} style={paperStyle}>
+        <Grid
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Avatar style={avatarStyle}></Avatar>
+          <h2 style={headerStyle}>Sing Up</h2>
+          <Typography variant="caption">
+            Please fill this form to create an account
+          </Typography>
+        </Grid>
+        <form>
+          <TextField
+            fullWidth
+            label="Name"
+            placeholder="Enter Yout Name..."
+            style={{ margin: "10px auto" }}
+          />
+          <TextField
+            fullWidth
+            label="Phone"
+            placeholder="Enter Yout Phone..."
+            style={{ margin: "10px auto" }}
+          />
+          <Button
+            variant="contained"
+            style={{
+              backgroundColor: "#FF9494",
+              color: "white",
+              fontWeight: "bold",
+              margin: "50px auto",
+            }}
+            fullWidth
+          >
+            Sing Up
+          </Button>
+        </form>
+      </Paper>
+    </Grid>
+  );
+};
 
-export default Signup
+export default Signup;
