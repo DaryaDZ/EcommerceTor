03d6d3e9c58c8551bb6ab263b2d7a0c274c5d26e
YasNvd <yaasnvd@yahoo.com>
2023-01-14 11:39:59 +0330

    core

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Layout />} >
-          <Route path='/signin' element={<Login />} />
-          <Route path='/signup' element={<Signup />} />
+          <Route path='signin' element={<Login />} />
+          <Route path='signup' element={<Signup />} />
 
 
 
